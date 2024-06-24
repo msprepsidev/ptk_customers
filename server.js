3a7906d8aa09d94b5504c8538bcfc4606347b298
AmBetty <ambetty21@gmail.com>
2024-06-24 09:24:01 +0200

    UPD metrics and swagger

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express')
 const mongoose = require('mongoose')
 const customerRoutes = require( './routers/customerRoutes.js');
 const config = require('./config/config.js')
+const { app: metricsApp } = require('./utils/metrics');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./utils/swagger.yaml');
+
 
 const app = express();
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', customerRoutes);
+app.use(metricsApp);
+
 mongoose.connect(config.mongoURI, { useUnifiedTopology: true })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err));
